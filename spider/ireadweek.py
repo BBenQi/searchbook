e6aba8df1fa01bbfce355e1bacf145ec5d901d0d
Dwoud Liu <ynlbq@outlook.com>
2018-11-07 20:16:46 +0800

    添加 周读网页 解析

--- spider/ireadweek.py
+++ spider/ireadweek.py
@@ -18,10 +18,27 @@ class IreadSpider(threading.Thread):
         while not self.queue.empty():
             url = self.queue.get()
             html = requests.get(url).text
-            self.parse(html)
+            try:
+                self.parse(html)
+            except:
+                continue
 
     @staticmethod
     def parse(html):
         soup = BeautifulSoup(markup=html)
-        print(soup.title.string)
+        download = soup.select('body > div > div > div.hanghang-za > div.hanghang-box > div.hanghang-shu-content-btn '
+                               '> a')
+        author = soup.select('body > div > div > div.hanghang-za > div.hanghang-shu-content > '
+                             'div.hanghang-shu-content-font > p:nth-of-type(1)')
+        book_name = soup.select('body > div > div > div.hanghang-za > div:nth-of-type(1)')
+        book_type = soup.select('body > div > div > div.hanghang-za > div.hanghang-shu-content > '
+                                'div.hanghang-shu-content-font > p:nth-of-type(2)')
+        book_content = soup.select('body > div > div > div.hanghang-za > div.hanghang-shu-content > '
+                                   'div.hanghang-shu-content-font > p:nth-of-type(5)')
 
+        print('下载地址', download[0]['href'])
+        print('作者', author[0].string)
+        print('书籍名称', book_name[0].string)
+        print('类型', book_type[0].string)
+        print('简介', book_content[0].string)
+        print("*********************************")
