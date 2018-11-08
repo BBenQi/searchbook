a056f1f5a73a8aec942bdedae8ab9c491d4d3c5f
Dwoud Liu <ynlbq@outlook.com>
2018-11-08 08:39:18 +0800

    添加setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='searchbook',
+    version='',
+    packages=['dao', 'spider'],
+    url='',
+    license='',
+    author='liubenqiang',
+    author_email='',
+    description=''
+)
